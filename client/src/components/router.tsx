245d04c03e2b55bc725d31a5a10668c7d7430895
avraham <2721avi@gmail.com>
2024-12-20 01:51:16 +0200

    rout to incident-trends

--- client/src/components/router.tsx
+++ client/src/components/router.tsx
@@ -4,6 +4,7 @@ import CountryList from "./pages/analysis/CountryList";
 import CityList from "./pages/analysis/CityList";
 import Map from "./Map";
 import DeadliestAttackGraph from "./pages/analysis/DeadliestAttackGraph";
+import IncidentTrendsGraph from "./pages/analysis/IncidentTrendsGraph";
 
 const AppRouter: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const AppRouter: React.FC = () => {
       <Route path="/cities" element={<CityList />} />
       <Route path="/map" element={<Map />} />
       <Route path="/deadliest-attack" element={<DeadliestAttackGraph />} />
+      <Route path="/incident-trends" element={<IncidentTrendsGraph/>} />
     </Routes>
   );
 };
