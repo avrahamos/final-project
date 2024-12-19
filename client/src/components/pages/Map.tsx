5b6a54716769611840c98c237fdae079b43f512f
avraham <2721avi@gmail.com>
2024-12-19 11:29:27 +0200

    map componnet done

--- client/src/components/pages/Map.tsx
+++ client/src/components/pages/Map.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-export default function Map() {
-  return (
-    <div>Map</div>
-  )
+interface MapProps {
+  center: [number, number];
+  zoom: number;
+  markers: { latitude: number; longitude: number; popupText?: string }[];
 }
+
+const Map: React.FC<MapProps> = ({ center, zoom, markers }) => {
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {markers.map((marker, index) => (
+        <Marker key={index} position={[marker.latitude, marker.longitude]}>
+          {marker.popupText && <Popup>{marker.popupText}</Popup>}
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export default Map;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,23 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'], 
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], 
   theme: {
-    extend: {
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        },
-      },
-      animation: {
-        'fade-in': 'fadeIn 1s ease-in-out',
-      },
-      colors: {
-        militaryGreen: '#4b5320',
-        militaryBrown: '#6b4423',
-        militaryGray: '#5c5c5c',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
