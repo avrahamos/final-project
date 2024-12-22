22713022989a0c20ff4f1306cbca5d530e3f8fcd
avraham <2721avi@gmail.com>
2024-12-22 16:21:05 +0200

    Add io to server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,34 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import connectDB from "./config/db";
-import "dotenv/config";
+import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+
 import analysisRouter from './routers/analysisRouter'
 import relationshipsRouter from './routers/relationshipsRouter'
 
-const app = express();
+dotenv.config()
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+export const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: "*",
+  },
+});
+
+io.on("connection", (client) => {
+  console.log(`New socket connection: ${client.id}`);
+  client.on("disconnect", () => {
+    console.log(`Client disconnected: ${client.id}`);
+  });
+});
 
 connectDB();
 
