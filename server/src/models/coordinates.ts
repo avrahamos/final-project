49dbcdf002debd7f8c472963cc091a09c5bff810
avraham <2721avi@gmail.com>
2024-12-22 14:22:31 +0200

    CoordinatesSchema

---
+++ server/src/models/coordinates.ts
@@ -0,0 +1,23 @@
+import { Document, model, Schema } from "mongoose";
+
+ interface ICoordinates extends Document {
+  latitude: number;
+  longitude: number;
+  city?: string;
+  country: string;
+  region: string;
+}
+
+
+const CoordinatesSchema = new Schema<ICoordinates>({
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+  city: { type: String }, 
+  country: { type: String },
+  region: { type: String },
+});
+
+export const Coordinates = model<ICoordinates>(
+  "Coordinates",
+  CoordinatesSchema
+);
