4b3ca138175dedb925b0abbfd1f22a0199e1a9c1
avraham <2721avi@gmail.com>
2024-12-22 16:54:48 +0200

    setup Socket Events

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,12 +5,15 @@ import dotenv from "dotenv";
 import http from "http";
 import { Server } from "socket.io";
 
-import analysisRouter from './routers/analysisRouter'
-import relationshipsRouter from './routers/relationshipsRouter'
+import analysisRouter from "./routers/analysisRouter";
+import relationshipsRouter from "./routers/relationshipsRouter";
+import { setupSocketEvents } from "./socket/io";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 const app = express();
 
@@ -23,25 +26,18 @@ export const io = new Server(httpServer, {
   },
 });
 
-io.on("connection", (client) => {
-  console.log(`New socket connection: ${client.id}`);
-  client.on("disconnect", () => {
-    console.log(`Client disconnected: ${client.id}`);
-  });
-});
-
-connectDB();
+setupSocketEvents(io);
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/analysis/" , analysisRouter);
+app.use("/api/analysis/", analysisRouter);
 app.use("/api/relationships/", relationshipsRouter);
 
 app.get("/ping", (req: Request, res: Response) => {
   res.status(200).send("pong");
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`   server up and run visit at http://localhost:${PORT}`);
 });
---
+++ server/src/socket/io.ts
@@ -0,0 +1,33 @@
+import { Server, Socket } from "socket.io";
+import { getAllCoordinates, searchCoordinates } from "../services/readLocationService";
+
+
+export const setupSocketEvents = (io: Server) => {
+  io.on("connection", (client: Socket) => {
+    console.log(`New client connected: ${client.id}`);
+
+    client.on("getAllCoordinates", async () => {
+      try {
+        const coordinates = await getAllCoordinates(); 
+        client.emit("coordinatesList", coordinates); 
+      } catch (error) {
+        console.error( error);
+        client.emit("error", "Failed to fetch coordinates");
+      }
+    });
+
+
+    client.on("searchCoordinates", async (query) => {
+      try {
+        const results = await searchCoordinates(query); 
+        client.emit("searchResults", results); 
+      } catch (error) {
+        console.error( error);
+        client.emit("error", "Failed to search coordinates");
+      }
+    });
+    client.on("disconnect", () => {
+      console.log(`Client disconnected: ${client.id}`);
+    });
+  });
+};
