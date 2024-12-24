54172e2f920c34d484a8a957f6af1d46f0b0cb15
avraham <2721avi@gmail.com>
2024-12-24 10:54:49 +0200

    searchByCityPrefix

--- server/src/services/coordinetsService.ts
+++ server/src/services/coordinetsService.ts
@@ -1,4 +1,5 @@
 import { Coordinates } from "../models/coordinates";
+import { ISummary, Summary } from "../models/summary";
 
 export const getAllCoordinates = async () => {
   try {
@@ -27,31 +28,30 @@ export const getAllCoordinates = async () => {
   }
 };
 
-
 export const searchCountries = async (query: { country?: string }) => {
   try {
     const match: any = {};
     if (query.country) match.country = { $regex: query.country, $options: "i" };
 
     const results = await Coordinates.aggregate([
-      { $match: match }, 
+      { $match: match },
       {
         $group: {
-          _id: { country: "$country", city: "$city" }, 
-          latitude: { $first: "$latitude" }, 
-          longitude: { $first: "$longitude" }, 
+          _id: { country: "$country", city: "$city" },
+          latitude: { $first: "$latitude" },
+          longitude: { $first: "$longitude" },
         },
       },
       {
         $group: {
-          _id: "$_id.country", 
+          _id: "$_id.country",
           cities: {
             $push: {
               city: "$_id.city",
               latitude: "$latitude",
               longitude: "$longitude",
             },
-          }, 
+          },
         },
       },
       {
@@ -68,5 +68,41 @@ export const searchCountries = async (query: { country?: string }) => {
   } catch (error) {
     console.error(error);
     throw new Error("failed to search");
+  }
+};
+export const getDocumentsByCountry = async (countryName: string) => {
+  try {
+    const documents :ISummary[]= await Summary.aggregate([
+      {
+        $match: {
+          country_txt: { countryName, $options: "i" },
+          latitude: { $exists: true, $ne: null },
+          longitude: { $exists: true, $ne: null },
+          city: { $exists: true, $ne: null  },
+        },
+      },
+    ]);
+
+    return documents;
+  } catch (error) {
+    console.error("Error fetching documents:", error);
+    throw new Error("Failed to fetch documents");
+  }
+};
+
+export const searchByCityPrefix = async (countryName:string, prefix: string) => {
+  try {
+    const documents: ISummary[] = await getDocumentsByCountry(countryName);
+    
+    const lowerCasePrefix = prefix.toLowerCase();
+    const matchingDocuments = documents.filter((doc) => {
+      const city = doc.city?.toLowerCase();
+      return city?.startsWith(lowerCasePrefix);
+    });
+
+    return matchingDocuments;
+  } catch (error) {
+    console.error(error);
+    throw new Error("failed search");
   }
 };
